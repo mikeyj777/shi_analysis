2e9da57993ba61bd64f570c0e04ebadb36f90b84
Michael James <mikeyj777@gmail.com>
2025-05-07 23:17:50 -0400

    finalize

--- add_params_to_results.py
+++ add_params_to_results.py
@@ -1,4 +1,4 @@
-from math import log10
+from math import log10, pi
 import pandas as pd
 
 # "HazardStudy" db table - index = StudyID
@@ -11,6 +11,9 @@ def get_data_from_pandas(elem):
         elem = elem.values[0]
     return elem
 
+params_to_find = ['StoragePressure', 'ExitPressure', 'CatastrophicRelease', 'DischargeDiameter', 'ReleaseOrientation']
+param_title_to_store = ['storage_pressure', 'exit_pressure', 'catastrophic_release', 'discharge_diameter', 'release_orientation']
+
 output = []
 for _, row in df.iterrows():
     row_for_iter = row.to_dict()
@@ -18,8 +21,16 @@ for _, row in df.iterrows():
     study_id = row_for_iter['study_id']
     study_id = get_data_from_pandas(study_id)
     study_row = haz_studies_df[haz_studies_df['StudyID'] == study_id]
-    press_psig = get_data_from_pandas(study_row['StoragePressure'])
-    row_out['press_psig'] = press_psig
+    for param, title in zip(params_to_find, param_title_to_store):
+        val = study_row[param]
+        val = get_data_from_pandas(val)
+        row_out[title] = val
+    row_out['pressure'] = row_out['storage_pressure']
+    if pd.isna(row_out['pressure']):
+        row_out['pressure'] = row_out['exit_pressure']
+    del row_out['storage_pressure']
+    del row_out['exit_pressure']
+
     output.append(row_out)
 
 df_out = pd.DataFrame(output)
